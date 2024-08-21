6b9641f47247e204d4252b43532f0b5bddfe7d69
Vinicius Ferreira <viniciuferreira32@gmail.com>
2024-08-21 10:52:18 -0300

    chore: adds eslint and prettier configuration

--- config/eslint-config/node.js
+++ config/eslint-config/node.js
@@ -1,6 +1,6 @@
 /** @type {import('eslint').Linter.Config} */
 module.config = {
-  extends: ['@rocketseat/eslint-config/next'],
+  extends: ['@rocketseat/eslint-config/node'],
   plugins: ['eslint-plugin-simple-import-sort'],
   rules: {
     'simple-import-sort/imports': 'error'
