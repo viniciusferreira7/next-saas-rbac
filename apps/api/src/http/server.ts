b5db614de4e5d16952604858cb20bc80d6190846
Vinicius Ferreira <viniciuferreira32@gmail.com>
2024-11-20 08:19:20 -0300

    feat: adds the organization's listing and detail route

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -20,6 +20,8 @@ import { resetPassword } from './routes/auth/reset-password'
 import { errorHandler } from './routes/error-handler'
 import { createOrganization } from './routes/orgs/create-organization'
 import { getMembership } from './routes/orgs/get-membership'
+import { getOrganization } from './routes/orgs/get-organization'
+import { getOrganizations } from './routes/orgs/get-organizations'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -68,6 +70,8 @@ app.register(resetPassword)
 app.register(authenticateWithGithub)
 app.register(createOrganization)
 app.register(getMembership)
+app.register(getOrganization)
+app.register(getOrganizations)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('HTTP server is running! 🚀')
