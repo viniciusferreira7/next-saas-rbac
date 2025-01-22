e9dc5828fb65e1534ddcaf67fb01ce3ece0a5a74
Vinicius Ferreira <viniciuferreira32@gmail.com>
2025-01-22 09:02:00 -0300

    feat: create organization-switcher component

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -1,3 +1,6 @@
+import { Slash } from 'lucide-react'
+
+import { OrganizationSwitcher } from './organization-switcher'
 import { ProfileButton } from './profile-button'
 import { LogoSvg } from './svg/logo'
 
@@ -6,6 +9,9 @@ export function Header() {
     <div className="mx-auto flex max-w-[1200px] items-center justify-between">
       <div className="flex items-center gap-3">
         <LogoSvg />
+
+        <Slash className="size-3 -rotate-[24deg] text-border" />
+        <OrganizationSwitcher />
       </div>
       <div className="flex items-center gap-4">
         <ProfileButton />
