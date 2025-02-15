ee76ec199bedf9fded62ee59671407a01f0fc7c6
Vinicius Ferreira <viniciuferreira32@gmail.com>
2025-02-15 10:52:57 -0300

    feat: add a verification if member can a create project

--- apps/web/src/app/(app)/org/[slug]/create-project/page.tsx
+++ apps/web/src/app/(app)/org/[slug]/create-project/page.tsx
@@ -1,4 +1,7 @@
 import type { Metadata } from 'next'
+import { redirect } from 'next/navigation'
+
+import { getAbility } from '@/auth/auth'
 
 import { ProjectForm } from './project-form'
 
@@ -6,7 +9,12 @@ export const metadata: Metadata = {
   title: 'Create project',
 }
 
-export default function CreateProjectPage() {
+export default async function CreateProjectPage() {
+  const { permissions } = await getAbility()
+
+  if (permissions?.cannot('create', 'Project')) {
+    redirect('/')
+  }
   return (
     <div className="space-y-4">
       <h1 className="text-2xl font-bold">Create project</h1>
